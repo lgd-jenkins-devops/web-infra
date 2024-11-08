8a4d14717dca0c387d2dcbe6d5805c88b05e7ff7
Luis <luis@10.10.5.100>
2024-11-08 10:27:24 -0500

    set credential

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "bucket" {
-  source = "git@github.com:lgd-jenkins-devops/terraform-modules.git//network?ref=v1.0.0"
+  source = "git@github.com:lgd-jenkins-devops/terraform-modules.git//bucket?ref=v1.0.0"
   name = var.bucket.name
   location = var.bucket.location
   project_id = var.project_id
