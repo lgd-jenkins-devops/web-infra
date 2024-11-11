75502a88e48dfc9a353675839942375a4efa8d85
Luis <luis@MacBook.local>
2024-11-10 22:44:02 -0500

    change lb  module name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "bucket" {
   project_id = var.project_id
 }
 
-module "bucket-load-balance" {
+module "load-balance" {
   source = "git@github.com:lgd-jenkins-devops/terraform-modules.git//load_balancer?ref=v1.0.0"
   depends_on = [module.bucket]
   type = "http-bucket"
